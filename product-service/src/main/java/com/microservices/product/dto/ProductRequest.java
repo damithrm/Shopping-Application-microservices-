ec10a1d16fa42c35625f9d55b0d616177224b68e
damithrm <damithrmdr@gmail.com>
2025-05-29 20:29:20 +0530

    developing

---
+++ product-service/src/main/java/com/microservices/product/dto/ProductRequest.java
@@ -0,0 +1,17 @@
+package com.microservices.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProductRequest {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
---
+++ product-service/src/main/java/com/microservices/product/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.microservices.product.service;
+
+import com.microservices.product.dto.ProductRequest;
+import com.microservices.product.dto.ProductResponse;
+import com.microservices.product.model.Product;
+import com.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product with id {} is saved", product.getId());
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductResponse> productResponses = products.stream()
+                .map(product -> new ProductResponse(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice()))
+                .toList();
+        log.info("Fetched all products: {}", productResponses);
+        return productResponses;
+    }
+}
